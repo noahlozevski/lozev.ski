8e7e093e5908dd4ac2a73584b000c3a71d67e870
noah lozevski <55212226+noahlozevski@users.noreply.github.com>
2020-11-06 18:06:47 -0500

    update

--- lozev.ski/src/main.js
+++ lozev.ski/src/main.js
@@ -1,8 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import App from "./App.vue"
+import vuetify from "./plugins/vuetify"
+import "roboto-fontface/css/roboto/roboto-fontface.css"
+import "material-design-icons-iconfont/dist/material-design-icons.css"
+import router from "./router"
+import store from "./store"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  router,
+  store,
+  render: h => h(App),
+}).$mount("#app")
